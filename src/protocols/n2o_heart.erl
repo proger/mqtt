ec5bc52e2fd30609f240deb271df859512ce7f1e
Namdak Tonpa <maxim@synrc.com>
2018-11-15 06:04:18 +0200

    zero ping pong protocol

--- src/protocols/n2o_heart.erl
+++ src/protocols/n2o_heart.erl
@@ -6,6 +6,10 @@ info({text,<<"PING">> = _Ping}=Message, Req, State) ->
     n2o:info(?MODULE,"PING: ~p",[Message]),
     {reply, {text, <<"PONG">>}, Req, State};
 
+info({text,<<>>}=Message, Req, State) ->
+    n2o:info(?MODULE,"PING: ~p",[Message]),
+    {reply, {text, <<>>}, Req, State};
+
 info({text,<<"N2O,",Process/binary>> = _InitMarker}=Message, Req, State) ->
     n2o:info(?MODULE,"N2O INIT: ~p",[Message]),
     n2o_proto:push({init,Process},Req,State,n2o_proto:protocols(),[]);
